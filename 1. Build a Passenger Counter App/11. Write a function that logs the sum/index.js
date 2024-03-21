b809b6a1fb4844cbc2ab885881a875428b9b15d3
Julio Gaekel <jriveragaekel@gmail.com>
2024-03-21 12:50:44 +0100

    Complete tasks

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -2,5 +2,11 @@ let lap1 = 34
 let lap2 = 33
 let lap3 = 36
 
-// Create a function that logs out the sum of all the lap times
+// Create a function that logs out the sum of all the lap times\
+function totalLapTime() {
+    let totalLap = lap1 + lap2 + lap3;
+    console.log(totalLap)
+}
+
+totalLapTime()
 
--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -1,9 +1,14 @@
 let lapsCompleted = 0
 
 // Create a function that increments the lapsCompleted variable with one
+function lapIncrease() {
+    lapsCompleted++;
+}
 // Run it three times
 
-
+lapIncrease()
+lapIncrease()
+lapIncrease()
 
 
 console.log(lapsCompleted)
