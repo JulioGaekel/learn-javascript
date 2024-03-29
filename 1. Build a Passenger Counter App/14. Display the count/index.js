9a72119e509c373a75b4cdb56ae40f1918abe822
Julio Gaekel <jriveragaekel@gmail.com>
2024-03-29 18:28:26 +0100

    Complete exercises

--- 1. Build a Passenger Counter App/14. Display the count/index.js
+++ 1. Build a Passenger Counter App/14. Display the count/index.js
@@ -2,11 +2,15 @@
 
 // change the count-el in the HTML to reflect the new count
 
+let countEl = document.getElementById("count-el");
+console.log(countEl)
+
 let count = 0
 
 // TODO: Complete task tomorrow
 function increment() {
     count = count + 1
+    countEl.innerText = count;
     console.log(count)
 }
 
